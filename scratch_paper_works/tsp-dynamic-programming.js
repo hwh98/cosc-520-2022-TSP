bb34ee325c0f6ef87de0c9f4545a2fd4c19886b1
AL <32211153+iluvjava@users.noreply.github.com>
2022-11-20 18:33:28 -0800

    >w<

--- scratch_paper_works/tsp-dynamic-programming.js
+++ scratch_paper_works/tsp-dynamic-programming.js
@@ -1,83 +1,120 @@
+/**
+ *  Author's Name: Hongda Li
+ *  This code is made for COSP 2022 Winter Term 1, final project. And I am Hongda Li. I don't like javascript, but I guess
+ *  I can deal with it. 
+ */
+
 (()=>{
     console.log("Dynamic Programming module is being loaded")
-    let DYNAMIC_TABLE = {};
+
+    const DYNAMIC_TABLE = {}
 
     /**
-     * A generator for subsets. It generates the subsets of size k in a sized n set, and the number it returns represents 
-     * binary instruction to generate the subsets. for examples, given n = 3, k = 2, it means to choose from a sets of 3 
-     * for all subsets of size 2, it will return: 
-     * - 11 -> the first one, the second one, but not the third one.
-     * - 101 -> the first and last one and not the second one. 
-     * - 110 -> The last 2 but not the third one. 
-     * @{param} {number} n : The size of the whole se that we want to choose the subsets from. 
+     * 
+     * @{param} {number} a : An array of uniqe elements to choose from. 
      * @{param} {number} k : The size of the subsets that we want to choose from. 
-     * @{param} {number} [off_set]: This is a recursive parameter that you should not touch. 
      * @{yield} {number}: A finger print encoding the choice of the subset. 
      */
-    function *k_combinatorics(n, k, off_set=0)
+    function *k_combinatorics(n, k)
     {
-        if (!(Number.isInteger(n) && Number.isInteger(k))) throw `n: ${n}; k: ${k} has to be integers.`
-        if (k > n) throw `n < k with ${n} < ${k}, combinator failed.`;
-        if (k == 1)
+        if (!Number.isInteger(n) || !Number.isInteger(k)) throw "n, k must be integer to use k_combinatorics fxn. ";
+        if (n <- k) throw " k > n and we have ${k} > ${n}. ";
+        function *k_combinatorics_recur(a, k, start_at, already_chosen)
         {
-            for (let I = off_set; I < n; I++)
-            {
-                yield 1 << I;
+            if (k == 0) {
+                yield already_chosen;
+                return;
             }
-            return // Terminates base case
-        }
-        for (let I = off_set; I <= n - k; I++) 
-        {   
-            sub_itr = k_combinatorics(n, k - 1, off_set=I + 1);
-            for (let itm = sub_itr.next(); !itm.done; itm = sub_itr.next())
+            n = a.length
+            for (let I = start_at; I <= n - k; I++)
             {
-                yield (1 << I) + itm.value;
+                already_chosen.push(a[I]);
+                yield* k_combinatorics_recur(a, k - 1, I + 1, already_chosen);
+                already_chosen.pop();
             }
         }
+        yield* k_combinatorics_recur(Array.from(Array(n).keys()), k, 0, []);
     }
 
+    
     /*
-    * A class that can generate finger prints for all the subsets of a given set
+    * A class that can generate finger prints for all the subsets of a given set. The set has to be a set and not some 
+    * kind of multi-set. 
     * with size n
     */
-    class SubSetGenerator
+    class SubsetGenerator
     {
-        constructor(n, elements)
+        /**
+         * The construct for the subsets generator. give it an array with unique elements as the set to take the s
+         * subsets from. 
+         */
+        constructor(elements)
         {
-            if (!Number.isInteger(n) || n <= 0)
-            {
-                throw `Number n=${n} is not an integer of it's positive`
-            }
             if (!Array.isArray(elements))
             {
                 throw `arguments has to be type of array. `
             }
-            this.n = n;
+            this.n = elements.length;
             this.elements = elements;
         }
 
         /**
          * Get a generator for the subset of size k for the given sets. 
          */
-        k_get(k)
+        *k_get(k)
         {
-            let fingerprint_gen = k_combinatorics(this.n, k);
-            for (let fg = fingerprint_gen.value; !fingerprint_gen.done; fg = fingerprint_gen.next())
+            let g = k_combinatorics(this.n, k);
+            for (let item = g.next(); !item.done; item = g.next())
             {
-                for (let I = 0; I < this.n; I++)
-                {
-                    
-                }
+                let res_arr = item.value.map(x => this.elements[x]);
+                yield res_arr;
             }
         }
         
-        select(the_set)
-        {
-
-            return null
-        }
     };
 
+    /**
+     * Perform the dynamic programming approach to figure out the solution for a directed graph with edge weight assignment. 
+     * `c` is the maps that maps a 2 elements 
+     */
+    function tsp_dynamic(c)
+    {
+
+    }
+
+//======================================================================================================================
+    /**
+     *  Doing some tests on the subset generator. 
+     */
+    function simple_test1()
+    {   
+        let g = k_combinatorics(5, 3);
+        let s = 0;
+        for (let item = g.next(); !item.done; item = g.next())
+        {
+            s++;
+        }
+        console.assert(s == 10, "simple_test1 failed. ");
+        console.log("Simple Test 1 passed. ");
+    }
+
+    /**
+     * A test function for unit testing. 
+     */
+    function simple_test2()
+    {
+        let gen = new SubsetGenerator([1, 2, 3, 4, 5]);
+        gen = gen.k_get(3);
+        let s = 0;
+        for (let item = gen.next(); !item.done; item = gen.next())
+        {
+            s++;
+        }
+        console.assert(s == 10, "simple_test2 failed. ");
+        console.log("Simple Test 1 passed. ");
+    }
+
+    simple_test1(); simple_test2();
 
 
 })();
