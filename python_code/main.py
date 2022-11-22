622999ede0739dbc042f346c66070af23d928e33
AL <32211153+iluvjava@users.noreply.github.com>
2022-11-22 15:03:57 -0800

    >w<

--- python_code/main.py
+++ python_code/main.py
@@ -4,18 +4,12 @@ def k_combinatorics(n, k):
     """
     Function makes a generator for all subset of size k in a subset of size n.
 
-    Parameters
-    ------
-    n: int
-        The size of the set we are looking at.
-    k: int
-        The size subsets from this set that you want to choose from.
-    Raise
-    -----
-        Error when k > n.
+    :param int n: The size of the set we are looking at.
+    :param int k: The size subsets from this set that you want to choose from.
+    :raise Exception: Error when k > n.
     """
     if not (isinstance(n, int) and isinstance(k, int)):
-        raise f"The given n, k: {n}, {k} is not both an instance of integers."
+        raise Exception("The given n, k: {n}, {k} is not both an instance of integers.")
     assert n >= k, f"k_combinator expects k <= n but have {k} <= {n}. "
 
     def inner_recur(a, k, start_at, already_chosen):
@@ -42,13 +36,47 @@ class DynamicTSP:
     ptable: dict
         A dictionary that maps a subset of vertices S and a set of vertices {i, j} to the shortest path the goes from
         i -> j using all vertices in set S exactly once.
-    ctable
+        Expected Types:
+        (sorted list, tuple of the directed edge) |-> List of integers of the path
+
+    ctable: dict
+        A dictionary that maps subset of vertices S and a set of vertices {i, j} in the graph to the cost of the
+        shortest path that goes from i -> j through every vertex in S exactly once.
+        (sorted list, tuple of the directed edge) |-> Integers representing the cost.
     """
     def __init__(this, cost_table):
         assert isinstance(cost_table, dict), "Give DynamicTSP an instance of dictionary as cost table please."
+        this.c = cost_table
         this.ptable = {}
         this.ctable = {}
+        this.k = 0
+
+    def construct_base(this):
+        """
+            The base case is when the set S has size of 2, containing only 2 vertices. In this case the cost of the path
+            that goes from i to j is just the cost of the edges linking i, j.
+        :return:
+            None.
+        """
+        for e, cost in this.c.items:
+            this.ctable[sorted(e), e] = cost
+            this.ptable[sorted(e), e] = list(e)
+        return None
+
+    def construct_subset(this, k):
+        """
+
+        :param k:
+        :return:
+        """
         pass
+
+    def perform(this, verbosity):
+        """
+        Perform the algorithm and print out stuff for checking.
+        :return:
+        
+        """
 
 
 
@@ -61,5 +89,6 @@ def main(name):
 if __name__ == '__main__':
     print("The TSP Dynamic programming module is being loaded. ")
     main('PyCharm')
+
 
 
---
+++ python_code/test.py
@@ -0,0 +1,19 @@
+
+def Test1():
+    """
+    Test basics functionality of the k_combinatorics sets with 2 concrete examples.
+    :return:
+    True means we passed it, else we didn't.
+    """
+    pass
+
+def Test2():
+    """
+    Test of the binomial coefficient properties are all satisfied for a set of size 5.
+    :return:
+    True means we passed it, else we didn't. 
+    """
+    pass
+
+if __name__ == "__main__":
+    pass
